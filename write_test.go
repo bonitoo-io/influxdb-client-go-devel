e64987df1cdfb341df6a28f4caf94534eacc08b3
vlastahajek <vlastimil.hajek@bonitoo.io>
2020-03-23 11:58:35 +0100

    Added initial WriteAPI unit tests

---
+++ write_test.go
@@ -0,0 +1,104 @@
+package client
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+type testClient struct {
+	lines   []string
+	options *Options
+	t       *testing.T
+}
+
+func (t *testClient) WriteAPI(org, bucket string) WriteApi {
+	return nil
+}
+
+func (t *testClient) Close() {
+
+}
+
+func (t *testClient) QueryAPI(org string) QueryApi {
+	return nil
+}
+
+func (t *testClient) postRequest(url string, body io.Reader, requestCallback RequestCallback, responseCallback ResponseCallback) error {
+	assert.Equal(t.t, url, fmt.Sprintf("%s/api/v2/write?bucket=my-bucket&org=my-org", t.ServerUrl()))
+	bytes, err := ioutil.ReadAll(body)
+	lines := strings.Split(string(bytes), "\n")
+	lines = lines[:len(lines)-1]
+	t.lines = append(t.lines, lines...)
+	return err
+}
+
+func (t *testClient) Options() *Options {
+	return t.options
+}
+
+func (t *testClient) ServerUrl() string {
+	return "http://locahost:8900"
+}
+
+func (t *testClient) Setup(username, password, org, bucket string) (*SetupResponse, error) {
+	return nil, nil
+}
+func (t *testClient) Ready() (bool, error) {
+	return true, nil
+}
+
+func genPoints(num int) []*Point {
+	points := make([]*Point, num)
+	rand.Seed(321)
+
+	t := time.Now()
+	for i := 0; i < len(points); i++ {
+		points[i] = NewPoint(
+			"test",
+			map[string]string{
+				"id":       fmt.Sprintf("rack_%v", i%100),
+				"vendor":   "AWS",
+				"hostname": fmt.Sprintf("host_%v", i%10),
+			},
+			map[string]interface{}{
+				"temperature": rand.Float64() * 80.0,
+				"disk_free":   rand.Float64() * 1000.0,
+				"disk_total":  (i/10 + 1) * 1000000,
+				"mem_total":   (i/100 + 1) * 10000000,
+				"mem_free":    rand.Float64() * 10000000.0,
+			},
+			t)
+		if i%10 == 0 {
+			t = t.Add(time.Second)
+		}
+	}
+	return points
+}
+
+func TestWriteApiImpl_Write(t *testing.T) {
+	client := &testClient{
+		options: DefaultOptions(),
+		t:       t,
+	}
+	client.options.BatchSize = 5
+	writeApi := newWriteApiImpl("my-org", "my-bucket", client)
+	points := genPoints(10)
+	for _, p := range points {
+		writeApi.Write(p)
+	}
+	writeApi.close()
+	require.Len(t, client.lines, 10)
+	for i, p := range points {
+		line := p.ToLineProtocol(client.options.Precision)
+		//cut off last \n char
+		line = line[:len(line)-1]
+		assert.Equal(t, client.lines[i], line)
+	}
+}
